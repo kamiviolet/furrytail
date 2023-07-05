afd3bb5545cc759582ad891066160e702fc1d6b5
kamiviolet <kamiviolet@gmail.com>
2023-07-05 20:36:43 +0100

    update next.config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+require('dotenv').config()
+
+const nextConfig = {
+    env: {
+        CAT_API: process.env.CAT_API,
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "cdn2.thecatapi.com",
+          port: "",
+        }
+      ]
+    }
+}
 
 module.exports = nextConfig
